0daa59d3835ee35ffc4d25602799d5c3fb4e1b23
Toluwalase987 <toluwalase987@gmail.com>
2023-10-27 23:04:34 +0100

    Latest Commit

--- page.php
+++ page.php
@@ -11,11 +11,12 @@
       </div>
     </div>
 
+    <div class="container container--narrow page-section">
+        
     <?php 
     $theParent = wp_get_post_parent_id(get_the_ID());
         if($theParent) { 
     ?>           
-            <div class="container container--narrow page-section">
               <div class="metabox metabox--position-up metabox--with-home-link">
                 <p>
                   <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent);?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent);?></a> <span class="metabox__main"><?php the_title();?></span>
@@ -25,6 +26,12 @@
     }
     ?>
 
+    <?php 
+    $testArray = get_pages(array(
+        'child_of' => get_the_ID(),
+    ));
+    
+    if($theParent or $testArray) { ?>
 
       <div class="page-links">
         <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent) ?>"><?php echo get_the_title($theParent); ?></a></h2>
@@ -38,9 +45,11 @@
           wp_list_pages(array(
             'title_li' => NULL,
             'child_of' => get_the_ID(),
+            'sort_column' => 'menu_order'
           )); ?>
         </ul>
       </div>
+      <?php } ?>
 
       <div class="generic-content">
         <?php the_content(); ?>

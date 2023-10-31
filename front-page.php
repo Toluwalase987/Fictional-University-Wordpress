30b56a303235b43e7e75b63bb332955cc9b44d16
Toluwalase987 <toluwalase987@gmail.com>
2023-10-31 23:25:31 +0100

    Latest Commit

--- front-page.php
+++ front-page.php
@@ -33,7 +33,7 @@
         <h1 class="headline headline--large">Welcome!</h1>
         <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
         <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-        <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+        <a href="<?php echo site_url('/programs') ?>" class="btn btn--large btn--blue">Find Your Major</a>
       </div>
     </div>
 
@@ -60,21 +60,9 @@
                 )
             ));
             while($homePageEvents->have_posts()){
-                $homePageEvents->the_post(); ?>
-                <div class="event-summary">
-                    <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month"><?php 
-                        $currentMonth = new DateTime(get_field('event_date'));
-                        echo $currentMonth->format('M')
-                     ?></span>
-                    <span class="event-summary__day"><?php echo $currentMonth->format('d') ?></span>
-                    </a>
-                    <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                    <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-                    </div>
-                </div>
-          <?php  }
+                $homePageEvents->the_post();
+                get_template_part('template-parts/content', 'event');
+           }
           ?>
 
 
--- single-program.php
+++ single-program.php
@@ -1,16 +1,10 @@
 
 <?php get_header();
     while(have_posts()){
-        the_post(); ?>
-         <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg');?>"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title();?></h1>
-        <div class="page-banner__intro">
-          <p>DON'T FORGET TO REPLACE ME LATER.</p>
-        </div>
-      </div>
-    </div>
+        the_post(); 
+        pageBanner();
+        ?>
+         
 
     <div class="container container--narrow page-section">
     <div class="metabox metabox--position-up metabox--with-home-link">
@@ -22,6 +16,75 @@
         <div class="generic-content">
             <?php the_content();?>
         </div>
+       
+        <?php 
+
+$today = date('Ymd');
+$relatedProfessors = new WP_Query(array(
+    'posts_per_page' => -1,
+    'post_type' => 'professor',
+    'orderby' => 'title',
+    'order' => 'ASC',
+    'meta_query' => array(
+        array(
+          'key', 'related_programs',
+          'compare' => 'LIKE',
+          'value' => '"' . get_the_ID() . '"'
+        )
+    )
+));
+
+  if($relatedProfessors->have_posts()){
+    echo '<hr class="section-break">';
+    echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professors</h2>';
+
+    echo '<ul class="professor-cards">';
+    while($relatedProfessors->have_posts()){
+        $relatedProfessors->the_post(); ?>
+        <li class="professor-card__list-item">
+          <a class="professor-card" href="<?php the_permalink(); ?>">
+              <img class="professor-card__image" src="<?php the_post_thumbnail(); ?>">
+              <span class="professor-card__name"><?php the_title(); ?></span>
+          </a>
+        </li>
+    <?php  }
+      echo  '</ul>';
+  }
+
+      wp_reset_postdata();
+
+            $today = date('Ymd');
+            $homePageEvents = new WP_Query(array(
+                'posts_per_page' => 2,
+                'meta_key' => 'event_date',
+                'post_type' => 'event',
+                'orderby' => 'meta_value_num',
+                'order' => 'ASC',
+                'meta_query' => array(
+                    array(
+                        'key' => 'event_date',
+                        'compare' => '>=',
+                        'value' => $today,
+                        'type' => 'numeric'
+                    ),
+                    array(
+                      'key', 'related_programs',
+                      'compare' => 'LIKE',
+                      'value' => '"' . get_the_ID() . '"'
+                    )
+                )
+            ));
+
+           if($homePageEvents->have_posts()){
+            echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
+
+            while($homePageEvents->have_posts()){
+                $homePageEvents->the_post();
+                get_template_part('template-parts/content-event');
+              }
+           }
+          ?>
     </div>
 
     <?php 
